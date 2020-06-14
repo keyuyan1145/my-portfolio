0196168645e4828aa105c877998b1733cf49e105
keyuyan1145 <keyuyan01@gmail.com>
2020-06-14 10:01:34 -0400

    remove an extra = sign

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -33,7 +33,7 @@ function randomFact() {
  * when the user clicks on the icon */
 function myFunction() {
   var x = document.getElementById("mynav");
-  if (x.className === "nav") {
+  if (x.className == "nav") {
     x.className += " responsive";
   } else {
     x.className = "nav";
