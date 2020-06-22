546f20a44982825e64f30e2996167655e3d92448
keyuyan1145 <keyuyan01@gmail.com>
2020-06-22 15:09:23 -0400

    removed unneccessary functions

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -39,47 +39,7 @@ function myFunction() {
   }
 }
 
-function getHello() {
-  console.log('Fetching a random message.');
-  
-  // The fetch() function returns a Promise because the request is asynchronous.
-  const responsePromise = fetch('/data');
-
-  // When the request is complete, pass the response into handleResponse().
-  responsePromise.then(handleResponse);
-}
-
-/**
- * Handles response by converting it to text and passing the result to
- * addQuoteToDom().
-*/
-function handleResponse(response) {
-  console.log('Handling the response.');
-
-  // response.text() returns a Promise, because the response is a stream of
-  // content and not a simple variable.
-  const textPromise = response.text();
-	
-  // When the response is converted to text, pass the result into the
-  // addQuoteToDom() function.
-  textPromise.then(addToDom);
-}
-
-/** Adds a random message to the DOM. */
-function addToDom(message) {
-  console.log('Adding quote to dom: ' + message);
-  const messageContainer = document.getElementById('message-container');
-  messageContainer.innerText = message;
-}
-
-// async version of the get HellowMessage
-async function getHelloAwait() {
-  const response = await fetch('/data');
-  const message = await response.text();
-  document.getElementById('message-container').innerText = message;
-}
-
-// parsing & fetching json messages
+/** parsing & fetching json messages */
 async function getComment() {
   const response = await fetch('/data');
   const comments = await response.json();
